b8bb851b4676a96c7e932173e446e75da1bdf864
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToImplementSingleton.Module/ShowSingletonController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.Persistent.Base;
+
+namespace HowToImplementSingleton.Module {
+	public partial class ShowSingletonController : ViewController {
+		public ShowSingletonController() {
+			InitializeComponent();
+			RegisterActions(components);
+		}
+		private void popupWindowShowAction1_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e) {
+			ObjectSpace objectSpace = Application.CreateObjectSpace();
+			e.View = Application.CreateDetailView(objectSpace, Singleton.GetInstance(objectSpace.Session));
+		}
+	}
+}
